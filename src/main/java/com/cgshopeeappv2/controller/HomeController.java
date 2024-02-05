02a084882db5b643a3110556ebcc56ce96eb8031
ThoiAn2K <146901840+ThoiAn2K@users.noreply.github.com>
2024-02-05 22:28:31 +0700

    create bill, statistics of seller , voucher, history  and information of seller

--- src/main/java/com/cgshopeeappv2/controller/HomeController.java
+++ src/main/java/com/cgshopeeappv2/controller/HomeController.java
@@ -1,14 +1,17 @@
 package com.cgshopeeappv2.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-    @RequestMapping(value = {"/", "/home"})
+    @RequestMapping(value = {"/", "/info"})
     public ModelAndView home() {
-        ModelAndView modelAndView = new ModelAndView("content/result");
+        ModelAndView modelAndView = new ModelAndView("content/seller-information");
         return modelAndView;
     }
 }
+
